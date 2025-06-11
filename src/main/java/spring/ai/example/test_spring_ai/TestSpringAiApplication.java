fea7d652495337f90389ac4a6e2e79e184e2b4d2
Gino Santonicola <luigisantonicola1988@gmail.com>
2025-06-11 11:30:12 +0200

    first commit

---
+++ src/main/java/spring/ai/example/test_spring_ai/TestSpringAiApplication.java
@@ -0,0 +1,13 @@
+package spring.ai.example.test_spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/ai/example/test_spring_ai/TestSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ai.example.test_spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
